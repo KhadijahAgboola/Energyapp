1ae9db3a220f62be157b7d3398d2c430a18e75c9
Khadijat Agboola <khadijahagboola12@gmail.com>
2024-12-03 09:45:27 +0100

    added more packagess

--- energy.py
+++ energy.py
@@ -2,6 +2,8 @@ import streamlit as st
 from PIL import Image
 import streamlit as st
 import pandas as pd
+import numpy as np
+import sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
